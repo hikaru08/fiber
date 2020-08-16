f4fed40ada6f49ffea7b29946daea47c9b2b69e9
takuma10Mi <amaebilove@gmail.com>
2020-08-16 10:09:42 +0900

    commit

--- functions.php
+++ functions.php
@@ -16,7 +16,27 @@ function load_js() {
 		wp_enqueue_script( 'slick.min', get_stylesheet_directory_uri().'/slick/slick.min.js', array( 'jquery' ), '1.8.1', true);
 	}
 }
+function load_css() {
+	$dir = get_stylesheet_directory_uri();
+	wp_enqueue_style('town-css', $dir . '/stylesheets/header.css', array(), filemtime(get_stylesheet_directory() . '/stylesheets/header.css'));
+	wp_enqueue_style( 'original-styles', get_stylesheet_directory_uri() . '/stylesheets/header.css', array(),  true);
+	wp_enqueue_style( 'reset', get_template_directory_uri() . '/stylesheets/reset.css', "", '20200804' );
+	wp_enqueue_style( 'header', get_template_directory_uri() . '/stylesheets/header.css', "", '20200804' );
+	wp_enqueue_style( 'index', get_template_directory_uri() . '/stylesheets/index.css', "", '20200804' );
+	wp_enqueue_style( 'creator', get_template_directory_uri() . '/stylesheets/creator.css', "", '20200804' );
+	wp_enqueue_style( 'member', get_template_directory_uri() . '/stylesheets/member.css', "", '20200804' );
+	wp_enqueue_style( 'works', get_template_directory_uri() . '/stylesheets/works.css', "", '20200804' );
+	wp_enqueue_style( 'works-detail', get_template_directory_uri() . '/stylesheets/works-details.css', "", '20200804' );
+	wp_enqueue_style( 'airticle', get_template_directory_uri() . '/stylesheets/article.css', "", '20200804' );
+	wp_enqueue_style( 'page', get_template_directory_uri() . '/stylesheets/page.css', "", '20200804' );
+	wp_enqueue_style( 'contact', get_template_directory_uri() . '/stylesheets/contact.css', "", '20200804' );
+	wp_enqueue_style( 'slider-zoom', get_template_directory_uri() . '/stylesheets/slider-zoom.css', "", '20200804' );
+	wp_enqueue_style( 'company', get_template_directory_uri() . '/stylesheets/company.css', "", '20200804' );
+	wp_enqueue_style( 'slick', get_template_directory_uri() . '/slick/slick.css', "", '20200804' );
+	wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/slick/slick-theme.css', "", '20200804' );
+}
 add_action( 'init', 'load_js' );
+add_action( 'css' , 'load_css');
 // ブログのアイキャッチ画像を使用可能にする
 add_theme_support('post-thumbnails');
 // カスタムメニューを使用可能にする
