20d39f66f9c6227703fc68ffe4e1956280b39192
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-10 16:37:04 +0900

    Revert "first commit"
    This reverts commit 3d4bac572bcaeee759d292af1a7fe3a45ffa8a75.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header()?>
-<?php get_template_part('');?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/stylesheets/opening.css" />
+<?php get_template_part('template/top_main');?>
+<?php get_template_part('template/top_concept');?>
+<?php get_template_part('template/top_creator');?>
+<?php get_template_part('template/top_works');?>
+<?php get_template_part('template/top_blog');?>
 <?php get_footer()?>
